3e9bbc7eb7ab66fca090839cd86498ec40b2408c
Eric Denovellis <edeno@bu.edu>
2019-09-26 08:25:00 -0700

    Add analysis and setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from setuptools import find_packages, setup
+
+INSTALL_REQUIRES = []
+TESTS_REQUIRE = []
+
+setup(
+    name='pose_analysis',
+    version='0.1.0.dev0',
+    license='',
+    description=(''),
+    author='',
+    author_email='',
+    url='',
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+)
