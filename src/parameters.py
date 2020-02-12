3fb3d36c2a8bfda3b0724a685e03f216f211bd03
Eric Denovellis <edeno@bu.edu>
2020-02-12 07:36:43 -0800

    Add parameters for classifiers.

--- src/parameters.py
+++ src/parameters.py
@@ -1,5 +1,7 @@
 from os.path import abspath, dirname, join, pardir
 
+import numpy as np
+
 from loren_frank_data_processing import Animal
 
 # LFP sampling frequency
@@ -18,3 +20,41 @@ ANIMALS = {
 
 EDGE_ORDER = [0, 1, 3, 2, 4]
 EDGE_SPACING = [15, 0, 15, 0]
+
+detector_parameters = {
+    'movement_var': 6.0,
+    'replay_speed': 1,
+    'place_bin_size': 2.5,
+    'spike_model_knot_spacing': 8.0,
+    'spike_model_penalty': 0.5,
+    'movement_state_transition_type': 'random_walk',
+    'multiunit_model_kwargs': {
+        'bandwidth': np.array([20.0, 20.0, 20.0, 20.0, 8.0])},
+    'multiunit_occupancy_kwargs': {'bandwidth': np.array([8.0])},
+    'discrete_state_transition_type': 'constant',
+    'discrete_diagonal': np.array([0.00003, 0.968])
+}
+
+classifier_parameters = {
+    'movement_var': 6.0,
+    'replay_speed': 1,
+    'place_bin_size': 2.5,
+    'continuous_transition_types': [['random_walk', 'uniform'],
+                                    ['uniform',     'uniform']],
+    'model_kwargs': {
+        'bandwidth': np.array([20.0, 20.0, 20.0, 20.0, 8.0])}
+
+}
+
+discrete_state_transition = np.array([[0.968, 0.032],
+                                      [0.032, 0.968]])
+
+'''
+1. Geometric mean of duration is 1 / (1 - p)
+2. So p = 1 - (1 / n_time_steps).
+3. Want `n_time_steps` to equal half a theta cycle.
+4. Theta cycles are ~8 Hz or 125 ms per cycle.
+5. Half a theta cycle is 62.5 ms.
+6. If our timestep is 2 ms, then n_time_steps = 31.25
+7. So p = 0.968
+'''
